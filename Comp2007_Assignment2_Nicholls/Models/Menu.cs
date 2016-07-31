d951016a30886642193acde3df73eb070309a750
AlexandraNicholls <alex@nicholls.ws>
2016-07-31 12:39:43 -0400

    Added menu model, trying to get the views ready

---
+++ Comp2007_Assignment2_Nicholls/Models/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Comp2007_Assignment2_Nicholls.Models
+{
+    public class Menu
+    {
+        //Add the columns for the menu
+        public string Name { get; set; }
+        public string 
+    }
+}
