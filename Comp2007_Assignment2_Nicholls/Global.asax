5b8d7a53ef54c6adcb22a350daaf94497f8b2730
AlexandraNicholls <alex@nicholls.ws>
2016-07-31 12:17:22 -0400

    Add project files.

---
+++ Comp2007_Assignment2_Nicholls/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Comp2007_Assignment2_Nicholls.MvcApplication" Language="C#" %>
---
+++ Comp2007_Assignment2_Nicholls/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Comp2007_Assignment2_Nicholls.Startup))]
+namespace Comp2007_Assignment2_Nicholls
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
